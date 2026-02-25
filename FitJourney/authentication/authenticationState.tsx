0e1e4a0afe69eaa94e1aef405136ac480cef72d9
Alessandro Poggio <alessandro.poggio@helloprint.com>
2026-02-25 19:36:50 +0100

    refactor: clean up authenticationState component by removing unused imports and web-specific code

--- FitJourney/authentication/authenticationState.tsx
+++ FitJourney/authentication/authenticationState.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Platform } from 'react-native';
-import auth, { getAuth, onAuthStateChanged, FirebaseAuthTypes } from '@react-native-firebase/auth';
-import { getApp, initializeApp } from '@react-native-firebase/app';
+import { View, Text } from 'react-native';
+import { getAuth, onAuthStateChanged, FirebaseAuthTypes } from '@react-native-firebase/auth';
 
 function App() {
   // Set an initializing state whilst Firebase connects
@@ -20,23 +19,6 @@ function App() {
   }, []);
 
   if (initializing) return null;
-  
-  if (Platform.OS === 'web') {
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-    const firebaseConfig = {
-      apiKey: "AIzaSyBYwVXZZE5DiOOCxfMktgUvwP6IwRaAykk",
-      authDomain: "fitjourney-at2023.firebaseapp.com",
-      projectId: "fitjourney-at2023",
-      storageBucket: "fitjourney-at2023.firebasestorage.app",
-      messagingSenderId: "1182679697",
-      appId: "1:1182679697:web:65ba706dbfa1691d7cf530",
-      measurementId: "G-MJV87XYTQZ"
-    };
-
-    const app = initializeApp(firebaseConfig);
-  };
 
   if (!user) {
     return (
@@ -53,4 +35,3 @@ function App() {
   );
 }
 
-export default App;
