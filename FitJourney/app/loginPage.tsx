1a4901e24886cdffa0d610a1966b64fff4bbe663
Hugo Maurinier <hugo.maurinier@epitech.eu>
2026-03-18 19:57:59 +0100

    feat: trigger firebase authentication on login form submission

--- FitJourney/app/loginPage.tsx
+++ FitJourney/app/loginPage.tsx
@@ -1,29 +1,38 @@
-import { StyleSheet } from "react-native";
-import React from "react";
-import { ThemedView } from "@/components/themed-view";
-import { Stack, useRouter } from "expo-router";
 import LoginForm from "@/components/login-form";
-
+import { ThemedView } from "@/components/themed-view";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "@react-native-firebase/auth";
+import { Stack, useRouter } from "expo-router";
+import React from "react";
+import { StyleSheet } from "react-native";
 
 export default function LoginScreen() {
   const router = useRouter();
 
+  const handleLogin = async (values: Record<string, any>) => {
+    const { username, password } = values;
+    try {
+      await signInWithEmailAndPassword(getAuth(), username, password);
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  };
+
   return (
     <ThemedView style={styles.container}>
-      <Stack.Screen
-        options={{ title: "Login" }}
-      />
+      <Stack.Screen options={{ title: "Login" }} />
       <ThemedView>
         <LoginForm
-            title="Login to FitJourney"
-            fields={[
-                { name: "username", label: "Username", type: "text" },
-                { name: "password", label: "Password", type: "password" },
-            ]}
-        >
-        </LoginForm>        
+          title="Login to FitJourney"
+          fields={[
+            { name: "username", label: "Username", type: "text" },
+            { name: "password", label: "Password", type: "password" },
+          ]}
+          onSubmit={handleLogin}
+        ></LoginForm>
       </ThemedView>
-
     </ThemedView>
   );
 }
